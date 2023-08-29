12e071b987c474838bf507d40b1ed74fb250d1dc
weinanliu <44917714+weinanliu@users.noreply.github.com>
2023-08-29 14:30:50 +0800

    Update create_sshded_docker.sh

--- create_sshded_docker.sh
+++ create_sshded_docker.sh
@@ -50,6 +50,9 @@ docker pull ${IMAGE_NAME}
 
 docker build --no-cache -t ${SSHDED_IMAGE} .
 
+# --shm-size=1g这个flag为了支持nccl。nccl库需要很大的共享内存
+# https://github.com/NVIDIA/nccl-tests/issues/143
+
 docker run \
 	 -d \
 	 --shm-size=1g \
